f891edb48dc9345ab94582abcf623da41508dc18
Longwell <Longwell@longwell.local>
2017-05-28 23:44:34 -0400

    Got the very basics of the code working

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,51 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var sampleInput = 1300
+
+var ottawaTime = sampleInput
+
+var BCTime = sampleInput - 300
+
+var edmontonTime = sampleInput - 200
+
+var winepegTime = sampleInput - 100
+
+var torontoTime = sampleInput
+
+var halifaxTime = sampleInput + 100
+
+var stJohnTime = sampleInput + 130
+
+//if sampleInput > 2229{
+//    
+//    stJohnTime -= 2360
+//    
+//}else if sampleInput >= 2259{
+//    
+//    
+//}
+
+
+
+
+print("\(ottawaTime) in Ottawa")
+print("\n")
+print("\(BCTime) in Victoria")
+print("\n")
+print("\(edmontonTime) in Edmonton")
+print("\n")
+print("\(winepegTime) in Winnipeg")
+print("\n")
+print("\(torontoTime) in Toronto")
+print("\n")
+print("\(halifaxTime) in Halifax")
+print("\n")
+print("\(stJohnTime) in St. John's")
+
+
+
+
+
+
